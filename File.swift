6f4211cc2fa7aa6a227348ef42638d939b6aeebd
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2021-04-13 21:16:52 +0200

    Update File.swift
    Apply team decisions

--- File.swift
+++ File.swift
@@ -1,49 +1,95 @@
+//  Created by Tomasz Mędryk on 25/03/15.
+//  Copyright (c) 2015 smartpatient. All rights reserved.
+
 import Foundation
 import UIKit
 
-protocol Functionality {
+// sourcery: AutoMockable
+protocol Functionality  {
     func foo()
     var value: Int { get set }
 }
 
+// Use Impl suffix in the class name only when there is a dedicated protocol defining the interface
 class FunctionalityImpl {
+    
+    // Place Constant enum at the top of the file
+    private enum Constants {
+        static let key = ""
+    }
+    
+    // MARK: - Dependencies
+    
+    private let someDependency: SomeDependency
     
     // MARK: - Initializers
     
-    init() {
-        self.a = 5
-    }
-    
-    init(a: Int) {
-        self.a = a
+    init(someDependency: SomeDependency) {
+        self.someDependency = someDependency
     }
     
     // MARK: - Functionality
     
+    // Keep the same order as in the protocol definition
     func foo() {
     }
     
     var value: Int
     
-    // MARK: - Dependencies
-    
-    private let a: Int
-    
     // MARK: - Privates
     
-    private enum Constants {
-        static let key = ""
-    }
-    
+    // Place helper methods on the bottom of the file
     private func helperCalculation() {
     }
     
 }
 
 /////////////////////////
-import SnapKit
 
-class FunctionalityView: UIView {
+class FunctionalitySpec: QuickSpec {
+    // swiftlint:disable:next function_body_length
+    override func spec() {
+        describe("Functionality") {
+            var sut: Functionality!
+            var someDependencyMock: SomeDependencyMock!
+            
+            beforeEach {
+                someDependency = SomeDependencyMock()
+                sut = FunctionalityImpl(someDependency: someDependency)
+            }
+            
+            context("when ...") {
+                beforeEach {
+                    sut.foo()
+                }
+                
+                it("should ...") {
+                    expect(sut.value) == 999
+                }
+                
+                it("should ...") {
+                    expect(someDependency...) == 999
+                }
+            }
+        }
+    }
+}
+
+/////////////////////////
+
+//  Created by Tomasz Mędryk on 25/03/15.
+//  Copyright (c) 2015 smartpatient. All rights reserved.
+
+// Use SnapKit for defining constraints
+import SnapKit 
+
+// Add to SomeView all subviews and configure static parameters here
+class SomeView: UIView { 
+    
+    // Place Constant enum at the top of the file
+    private enum Constant { 
+        static let key = ""
+    }
     
     // MARK: - Initializers
     
@@ -56,27 +102,28 @@ class FunctionalityView: UIView {
     
     // MARK: - FunctionalityView
     
-    var title: String? {
+    // Use getters and setters for subview properties access 
+    // Do not expose the while subview to the interface until absolutely necessary
+    var title: String? { 
         get { label.text }
         set { label.text = newValue }
     }
     
     // MARK: - Privates
     
-    private enum Constant {
-        static let key = ""
-    }
-    
+    // Initialize and define the subview in one place
     private lazy var label = UILabel {
         let label = UILabel()
         label.tintColor = .red
         return label
     }()
     
+    // Dedicated configuration method for adding subviews
     private func addSubviews() {
         addSubview(label)
     }
     
+    // Dedicated configuration method for defining constraints
     private func setupLayout() {
         label.snp.make {
             $0.center.equalToSuperview()
@@ -85,28 +132,38 @@ class FunctionalityView: UIView {
 
 }
 /////////////////////////////////////
-class FunctionalityViewModel {}
+//  Created by Tomasz Mędryk on 25/03/15.
+//  Copyright (c) 2015 smartpatient. All rights reserved.
+
+class SomeViewModel {}
 
 class TableViewDelegate {}
 
-class FuncionalityViewController: UIViewController {
+class SomeViewController: UIViewController {
+    
+    // MARK: - Dependencies
+    
+    // Define the class dependencies above init
+    private let viewModel: FunctionalityViewModel
+    private let tableViewDelegate = TableViewDelegate()
     
     // MARK: - Initializers
     
-    init(viewModel: FunctionalityViewModel) {
+    init(viewModel: SomeViewModel, tableViewDelegate: TableViewDelegate) {
         self.viewModel = viewModel
+        self.tableViewDelegate = tableViewDelegate
     }
     
     required init?(coder: NSCoder) { nil }
     
-    // MARK: - FuncionalityViewController
+    // MARK: - SomeViewController
     
     var dismiss: (() -> Void)?
     
     // MARK: - View Lifecycle
     
     override func loadView() {
-        view = FunctionalityView()
+        view = SomeView()
     }
     
     override func viewDidLoad() {
@@ -124,20 +181,16 @@ class FuncionalityViewController: UIViewController {
         //
     }
     
-    // MARK: - Dependencies
-    
-    private let viewModel: FunctionalityViewModel
-    private let tableViewDelegate = TableViewDelegate()
-    
     // MARK: - Privates
     
-    private var functionalityView: FunctionalityView! {
-        view as? FunctionalityView
+    // Place helper methods on the bottom of the file
+    private var contentView: SomeView! {
+        view as? SomeView
     }
     
     private func configureSubviews() {
-        functionalityView.title = "test"
-        // functionalityView.tableView.delegate = tableViewDelegate
+        contentView.title = "test"
+        // contentView.tableView.delegate = tableViewDelegate
     }
 
 }
